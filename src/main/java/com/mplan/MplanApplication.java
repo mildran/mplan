1a43cffcbd190aa965cb9c793067d292f80d36b6
MARCELINO PEREZ LUNA <marcelino.perez.luna@gmail.com>
2026-09-11 22:32:55 +0200

    Conf. Inicial

---
+++ src/main/java/com/mplan/MplanApplication.java
@@ -0,0 +1,13 @@
+package com.mplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mplan/MplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
